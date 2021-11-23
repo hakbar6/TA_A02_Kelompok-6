e715c74b81f3df385e10a4a5e6795736190cd41e
harakan akbar <harakanakbar2@gmail.com>
2021-11-23 22:30:40 +0700

    init:tugas akhir - first push

---
+++ SI-RETAIL/src/main/java/APAP/tugasAkhir/SIRETAIL/SiRetailApplication.java
@@ -0,0 +1,13 @@
+package APAP.tugasAkhir.SIRETAIL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiRetailApplication.class, args);
+	}
+
+}
---
+++ SI-RETAIL/src/test/java/APAP/tugasAkhir/SIRETAIL/SiRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package APAP.tugasAkhir.SIRETAIL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
