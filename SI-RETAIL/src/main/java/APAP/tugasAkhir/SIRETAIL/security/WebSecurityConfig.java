8294519e652dea7a949970663126102f123938fb
harakan akbar <harakanakbar2@gmail.com>
2021-12-24 16:00:35 +0700

    fixing database

--- SI-RETAIL/src/main/java/APAP/tugasAkhir/SIRETAIL/security/WebSecurityConfig.java
+++ SI-RETAIL/src/main/java/APAP/tugasAkhir/SIRETAIL/security/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v3/api-docs/**").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/user/create").hasAuthority("Kepala Retail")
-                .antMatchers("/cabang/create").hasAnyAuthority("Manager Cabang", "Kepala Retail")
+//                .antMatchers("/cabang/create").hasAnyAuthority("Manager Cabang", "Kepala Retail")
                 .antMatchers("/cabang/update/**").hasAnyAuthority("Manager Cabang", "Kepala Retail")
                 .antMatchers("/cabang/delete/**").hasAnyAuthority("Manager Cabang", "Kepala Retail")
                 .antMatchers("/cabang/daftarPermintaan").hasAnyAuthority("Kepala Retail")
